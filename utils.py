63370ded80413b62bb49c4a350be1fe2191e26e1
Hanzhen Lu <930883963@qq.com>
2023-05-16 14:01:46 +0800

    reproduce the base CodeT5 and record the score

---
+++ utils.py
@@ -0,0 +1,8 @@
+# Copyright (c) Microsoft Corporation. 
+# Licensed under the MIT license.
+import numpy as np
+ 
+def get_model_size(model):
+    model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+    model_size = sum([np.prod(p.size()) for p in model_parameters])
+    return "{}M".format(round(model_size / 1e+6))       
